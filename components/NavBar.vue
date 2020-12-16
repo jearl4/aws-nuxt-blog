e5e89467ba1628c00f777f4d11e19a06874bb4f7
John Earl <jtearl188@gmail.com>
2020-12-15 21:18:55 -0500

    switching branch to handle seo issue

---
+++ components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <a href="https://prismic.io" target="_blank" rel="noopener">
+      <img class="logo" src="../assets/img/prismic-logo.svg" alt="Prismic">
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style lang="sass" scoped>
+.footer
+  padding: 40px 0
+  max-width: 700px
+  margin: 0 auto
+  text-align: center
+  border-top: 1px solid #DADADA
+
+.logo
+  width: 100px
+</style>
