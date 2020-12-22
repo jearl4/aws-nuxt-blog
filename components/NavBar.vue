32a3b66bdd79d71bb322bf6a089e09736a1aac94
John Earl <jtearl188@gmail.com>
2020-12-21 21:03:36 -0500

    adding all dummy pages for each nav bar item

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,41 +1,49 @@
 <template>
   <v-navigation-drawer permanent expand-on-hover>
     <v-list>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon>mdi-home</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="nav-title-text">
-          Document Object
-        </v-list-item-title>
-      </v-list-item>
+      <NuxtLink to="/">
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="nav-title-text">
+            Document Object
+          </v-list-item-title>
+        </v-list-item>
+      </NuxtLink>
     </v-list>
     <v-divider></v-divider>
     <v-list nav dense>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon class="aws-icon">mdi-aws</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="nav-title-text">
-          Cloud Articles
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon class="typescript-icon">mdi-language-typescript</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="nav-title-text">
-          TypeScript Articles
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon class="java-icon">mdi-language-java</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="nav-title-text">
-          Java Articles
-        </v-list-item-title>
-      </v-list-item>
+      <NuxtLink to="/aws">
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon class="aws-icon">mdi-aws</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="nav-title-text">
+            Cloud Articles
+          </v-list-item-title>
+        </v-list-item>
+      </NuxtLink>
+      <NuxtLink to="/typescript">
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon class="typescript-icon">mdi-language-typescript</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="nav-title-text">
+            TypeScript Articles
+          </v-list-item-title>
+        </v-list-item>
+      </NuxtLink>
+      <NuxtLink to="/java">
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon class="java-icon">mdi-language-java</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="nav-title-text">
+            Java Articles
+          </v-list-item-title>
+        </v-list-item>
+      </NuxtLink>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -48,7 +56,6 @@ export default {
 
 <style lang="sass" scoped>
 .nav-title-text
-
   padding-left: 1em
 
 .aws-icon
